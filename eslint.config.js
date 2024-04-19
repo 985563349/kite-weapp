846877d00f8c23520d0a0e36c349389e405e64da
王杰 <jie985563349@outlook.com>
2024-04-19 16:40:06 +0800

    build: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['miniprogram_dist', 'miniprogram_npm'],
+  },
+  {
+    files: ['scripts/**'],
+    languageOptions: {
+      global: globals.node,
+    },
+  },
+  {
+    files: ['src/**', 'example/**'],
+    languageOptions: {
+      globals: {
+        getApp: false,
+        getCurrentPages: false,
+        Page: false,
+        Component: false,
+        App: false,
+        wx: false,
+        Behavior: false,
+      },
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+];
