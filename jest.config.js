9b1238e97909d707668aa9ea46d6b7ca61c7dc1d
王杰 <jie985563349@outlook.com>
2024-04-24 16:43:17 +0800

    test: add test report

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ export default {
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['<rootDir>/src/**/test/**/*.spec.{js,ts}'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts}', '!**/test/**'],
+  coverageProvider: 'v8',
   snapshotSerializers: ['miniprogram-simulate/jest-snapshot-plugin'],
   transform: {
     '^.+\\.(t|j)s$': '@swc/jest',
