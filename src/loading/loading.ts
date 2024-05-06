2a585069e303dd9d40ff6c7067690d539fd46dac
王杰 <jie985563349@outlook.com>
2024-05-06 18:10:11 +0800

    feat: add loading component

---
+++ src/loading/loading.ts
@@ -0,0 +1,21 @@
+import KComponent from '../common/component';
+
+KComponent({
+  properties: {
+    kId: String,
+
+    size: String,
+
+    color: String,
+
+    vertical: String,
+
+    label: String,
+
+    style: String,
+
+    ariaRole: String,
+
+    ariaLabel: String,
+  },
+});
