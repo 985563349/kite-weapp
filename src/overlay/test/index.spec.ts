5baf930659f13de32bd0402494acc1902e99419c
王杰 <jie985563349@outlook.com>
2024-05-12 13:44:07 +0800

    feat: add overlay component

---
+++ src/overlay/test/index.spec.ts
@@ -0,0 +1,15 @@
+import path from 'node:path';
+import simulate from 'miniprogram-simulate';
+
+describe('overlay', () => {
+  const KOverlay = simulate.load(path.resolve(__dirname, '../overlay'), 'k-overlay', {
+    rootPath: path.resolve(__dirname, '../../'),
+  });
+
+  test('should render overlay add match snapshot', () => {
+    const comp = simulate.render(KOverlay);
+    comp.attach(document.createElement('parent-wrapper'));
+
+    expect(comp.toJSON()).toMatchSnapshot();
+  });
+});
