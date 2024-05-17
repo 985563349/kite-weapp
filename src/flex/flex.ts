7472be89ba9d470c0457507bbaf0288d64f50c7e
龚春铭 <gongchunming@ctirobot.com>
2024-05-17 18:27:24 +0800

    feat: add flex component

---
+++ src/flex/flex.ts
@@ -0,0 +1,19 @@
+import KComponent from '../common/component';
+
+KComponent({
+  properties: {
+    kId: String,
+
+    style: String,
+
+    vertical: Boolean,
+
+    wrap: String,
+
+    justify: String,
+
+    align: String,
+
+    gap: String,
+  },
+});
