4022d741c81a2f08f0589b5ce28d368c1fcec336
王杰 <jie985563349@outlook.com>
2024-05-30 14:29:34 +0800

    feat: add popup component

---
+++ src/popup/popup.ts
@@ -0,0 +1,62 @@
+import KComponent from '../common/component';
+import transition from '../mixins/transition';
+
+KComponent({
+  properties: {
+    kId: String,
+
+    style: String,
+
+    placement: {
+      type: String,
+      value: 'center',
+      observer: 'observePlacement',
+    },
+
+    shape: String,
+
+    zIndex: {
+      type: Number,
+      value: 11000,
+    },
+
+    overlay: {
+      type: Boolean,
+      value: true,
+    },
+
+    overlayClosable: {
+      type: Boolean,
+      value: true,
+    },
+
+    lockScroll: {
+      type: Boolean,
+      value: true,
+    },
+  },
+
+  behaviors: [transition()],
+
+  lifetimes: {
+    created() {
+      this.observePlacement();
+    },
+  },
+
+  methods: {
+    onClickOverlay() {
+      this.triggerEvent('click-overlay');
+
+      if (this.data.overlayClosable) {
+        this.triggerEvent('close');
+      }
+    },
+
+    observePlacement() {
+      const { placement } = this.data;
+
+      this.setData({ name: placement });
+    },
+  },
+});
