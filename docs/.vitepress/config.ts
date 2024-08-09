GCM6/ikun-weapp

docs: update document

docs/.vitepress/config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'iKun WeApp',
+  title: 'Kite Weapp',
   description: '微信小程序组件库',
+  lastUpdated: true,
+  metaChunk: true,
+  cleanUrls: true,
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/kite-weapp-logo.svg' }],
+    ['meta', { name: 'theme-color', content: '#3451b2' }],
+  ],
+
   themeConfig: {
+    logo: { src: '/kite-weapp-logo.svg', width: 24, height: 24 },
+
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '指南', link: '/guide/quick-start' },
@@ -27,9 +38,11 @@ export default defineConfig({
           text: '基础组件',
           items: [
             { text: 'Button 按钮', link: '/components/button' },
-            { text: 'Flex 弹性布局', link: '/components/flex' },
             { text: 'Grid 栅格', link: '/components/grid' },
+            { text: 'Icon 图标', link: '/components/icon' },
             { text: 'Space 间距', link: '/components/space' },
+            { text: 'Flex 弹性布局', link: '/components/flex' },
+            { text: 'Spin 加载', link: '/components/spin' },
           ],
         },
         {
@@ -49,9 +62,68 @@ export default defineConfig({
             { text: 'Toast 轻提示', link: '/components/toast' },
           ],
         },
+        {
+          text: '动画组件',
+          items: [{ text: 'Transition 过渡', link: '/components/transition' }],
+        },
       ],
     },
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/985563349/kite-weapp/tree/main/docs' },
+    ],
+
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    editLink: {
+      pattern: 'https://github.com/985563349/kite-weapp/tree/main/docs/:path',
+      text: '在 GitHub 上编辑此页面',
+    },
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+
+    outline: {
+      label: '页面导航',
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
+
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
   },
 });

985563349/kite-weapp

docs/.vitepress/config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'iKun WeApp',
+  title: 'Kite Weapp',
   description: '微信小程序组件库',
+  lastUpdated: true,
+  metaChunk: true,
+  cleanUrls: true,
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/kite-weapp-logo.svg' }],
+    ['meta', { name: 'theme-color', content: '#3451b2' }],
+  ],
+
   themeConfig: {
+    logo: { src: '/kite-weapp-logo.svg', width: 24, height: 24 },
+
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '指南', link: '/guide/quick-start' },
@@ -27,9 +38,11 @@ export default defineConfig({
           text: '基础组件',
           items: [
             { text: 'Button 按钮', link: '/components/button' },
-            { text: 'Flex 弹性布局', link: '/components/flex' },
             { text: 'Grid 栅格', link: '/components/grid' },
+            { text: 'Icon 图标', link: '/components/icon' },
             { text: 'Space 间距', link: '/components/space' },
+            { text: 'Flex 弹性布局', link: '/components/flex' },
+            { text: 'Loading 加载', link: '/components/loading' },
           ],
         },
         {
@@ -49,9 +62,73 @@ export default defineConfig({
             { text: 'Toast 轻提示', link: '/components/toast' },
           ],
         },
+        {
+          text: '动画组件',
+          items: [{ text: 'Transition 过渡', link: '/components/transition' }],
+        },
       ],
     },
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/985563349/kite-weapp/tree/main/docs' }],
+
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    editLink: {
+      pattern: 'https://github.com/985563349/kite-weapp/tree/main/docs/:path',
+      text: '在 GitHub 上编辑此页面',
+    },
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+
+    outline: {
+      label: '页面导航',
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
+
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+  },
+
+  markdown: {
+    languageAlias: {
+      wxml: 'html',
+      wxss: 'css',
+    },
   },
 });